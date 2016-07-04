29bb663e53a76e11fc169b1755f9de1c675fff3d
Archer <archerhuang@iii.org.tw>
2016-07-04 16:21:19 +0800

    modify 9x9.py

--- 9x9.py
+++ 9x9.py
@@ -1,3 +1,6 @@
 for i in range(1, 10):
 	for j in range(1, 10):
-		print str(i) + '*' + str(j) + '=' + str(i*j)
+		print str(i) + '*' + str(j) + '=' + str(i*j) + "\t",
+	print ''
+
+#print "%d * %d = %d \t" % (i, j, i*j),
