fffa8e2fda4237f20d3cc7bb040e9e86528efea3
ArcherHuang <mmosconii@gmail.com>
2017-09-21 10:44:08 +0800

    Modify Call API HTTPS Code

--- call_API_GET.py
+++ call_API_GET.py
@@ -1,6 +1,6 @@
 import httplib
 conn = httplib.HTTPSConnection("us.wio.seeed.io", 443)
-conn.request("GET", "/v1/node/GroveMoistureA0/moisture?access_token=6b186b35b03cf3f1f17b9c09b93c094b")
+conn.request("GET", "/v1/node/GroveMoistureA0/moisture?access_token=")
 r1 = conn.getresponse()
 print r1.status, r1.reason
 data1 = r1.read()
